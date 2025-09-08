e028232aede2e6f7ce8474ef7764d45c16fb49df
Keltzu <kalleto@metropolia.fi>
2025-09-08 17:06:42 +0300

    Observer

---
+++ observer/src/WeatherObserver.java
@@ -0,0 +1,16 @@
+public class WeatherObserver implements Observer {
+    private final String name;
+
+    public WeatherObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(int temperature) {
+        System.out.println(name + " received update: The current temperature is " + temperature + "°C");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
