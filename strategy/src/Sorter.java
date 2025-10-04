5e0257450e38d94360e3f8f9cf14aac3468c9520
Keltzu <kalleto@metropolia.fi>
2025-10-04 17:09:58 +0300

    strategy

---
+++ strategy/src/Sorter.java
@@ -0,0 +1,15 @@
+public class Sorter {
+    private SortingStrategy strategy;
+
+    public void setStrategy(SortingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void sortArray(int[] array) {
+        long startTime = System.nanoTime();
+        strategy.sort(array);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000;
+        System.out.println(strategy.getName() + " took " + duration + " ms");
+    }
+}
