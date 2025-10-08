4495839c13e284f6ec7bca6b000d1b02182eaae0
Keltzu <kalleto@metropolia.fi>
2025-10-08 14:31:44 +0300

    fly

---
+++ flyweight/src/TreeImage.java
@@ -0,0 +1,19 @@
+public final class TreeImage implements Flyweight {
+    private final String image;
+    private final String description;
+
+    public TreeImage(String image, String description) {
+        this.image = image;
+        this.description = description;
+    }
+
+    @Override
+    public String getImage() {
+        return image;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ flyweight/src/TreeImageFactory.java
@@ -0,0 +1,19 @@
+public class TreeImageFactory {
+    public static final String LEAFLESS = "leafless";
+    public static final String LEAFY = "leafy";
+
+    private final TreeImage leaflessTreeImage;
+    private final TreeImage leafyTreeImage;
+
+    public TreeImageFactory() {
+        this.leaflessTreeImage = new TreeImage("L", "leafless birch");
+        this.leafyTreeImage = new TreeImage("G", "leafy birch");
+    }
+
+    public TreeImage getTreeImage(String type) {
+        if (LEAFY.equalsIgnoreCase(type)) {
+            return leafyTreeImage;
+        }
+        return leaflessTreeImage;
+    }
+}
