9d88ea2e966b161a9c53f776fe1ef5d9210620c4
Keltzu <kalleto@metropolia.fi>
2025-10-08 13:02:20 +0300

    build

---
+++ builder/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        ComputerBuilder gamingBuilder = new GamingComputerBuilder();
+        ComputerDirector director = new ComputerDirector(gamingBuilder);
+
+        director.constructComputer();
+        Computer gamingComputer = director.getComputer();
+        System.out.println("Gaming Computer built:");
+        System.out.println(gamingComputer);
+
+        System.out.println();
+
+        ComputerBuilder officeBuilder = new OfficeComputerBuilder();
+        director = new ComputerDirector(officeBuilder);
+
+        director.constructComputer();
+        Computer officeComputer = director.getComputer();
+        System.out.println("Office Computer built:");
+        System.out.println(officeComputer);
+    }
+}
